57923bac2510910fe0ecf1f22be81b3a15640ebb
Alexandre Lavoie <alexandre.lavoie00@gmail.com>
2024-05-31 14:47:01 -0400

    Leak OS

--- index.lua
+++ index.lua
@@ -5,3 +5,30 @@ crash_button.on_click(function ()
         crash_button.on_click(function () end)
     end
 end)
+
+local leak_button = get("leak-button")
+local leak_result = get("leak-result")
+leak_result.set_opacity(0)
+
+function file_exists(path)
+    f, err = loadfile(path)
+    return not string.find(err, "No such file")
+end
+
+leak_button.on_click(function ()
+    leak_result.set_opacity(0)
+
+    local os = "?"
+
+    if file_exists("C:\\Windows\\System32\\cmd.exe") then
+        os = "Windows"
+    elseif file_exists("/usr/lib/libc.so") then
+        os = "Arch Linux"
+    elseif file_exists("/etc/passwd") then
+        os = "Linux"
+    end
+
+    leak_result.set_content("Result: " .. os)
+
+    leak_result.set_opacity(1)
+end)
